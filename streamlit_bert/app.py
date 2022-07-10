92762330a9eadcd5cb49d5c73dd444dadea73050
Pradip Nichite <prnichite@gmail.com>
2022-07-10 15:26:50 +0530

    Deploy BERT with streamlit

---
+++ streamlit_bert/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
+    return tokenizer,model
+
+
+tokenizer,model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
+
+d = {
+    
+  1:'Toxic',
+  0:'Non Toxic'
+}
+
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
---
+++ streamlit_bert/requirements.txt
@@ -0,0 +1,3 @@
+streamlit
+torch
+transformers
